517c441665699fce47afff9ac2459cf6a3500635
DESKTOP-FRK722U\Thilina <dilshanthilina53@gmail.com>
2021-04-08 00:42:38 +0530

    better

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
-import Link from 'next/link'
+import Link from "next/link";
+import {useRouter} from 'next/router'
 
 const HomePage = () => {
+  const router = useRouter()
+  const gotoNext = ()=>{
+    router.push({pathname:'/about'})
+  }
   return (
     <div>
       <h1> The Home Page </h1>
@@ -8,13 +13,15 @@ const HomePage = () => {
         <li>
           <Link href="/portfolio"> PortFoliio </Link>
         </li>
-        <li>
-        <Link href="/clients"> Clients </Link>
 
+        <li>
+          <Link href="/clients"> Clients </Link>
         </li>
       </ul>
+      <h1> This is the second part </h1>
+      <button onClick={gotoNext}> Show About Page </button>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
